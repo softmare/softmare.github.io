965903cd9a53a84d0cc6a320adb0673d2bde4f77
Taehyeong Kim <th.imk3@gmail.com>
2022-03-08 21:21:14 +0900

    update : react initialization

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './images/water_melon.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to my home. 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
